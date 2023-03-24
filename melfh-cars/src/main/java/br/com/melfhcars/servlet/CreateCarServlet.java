d362fdb45d95089b477bdae3bc1f5547e4cde13b
Leonardo <leonardoss2035@gmail.com>
2023-03-23 22:05:47 -0300

    Primeira aplicação web do projeto MELFH-Cars

---
+++ melfh-cars/src/main/java/br/com/melfhcars/servlet/CreateCarServlet.java
@@ -0,0 +1,7 @@
+package br.com.melfhcars.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+}
