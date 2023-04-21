1423044fce64702419ab179e2d816ee5d44e4dea
Leonardo <leonardoss2035@gmail.com>
2023-04-20 22:10:22 -0300

    Incrementando a tela Home com a base de dados

--- melfh-cars/src/main/java/br/com/melfhcars/servlet/ListCarHome.java
+++ melfh-cars/src/main/java/br/com/melfhcars/servlet/ListCarHome.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/mostar-carro")
+@WebServlet("/mostrar-carro")
 public class ListCarHome extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
