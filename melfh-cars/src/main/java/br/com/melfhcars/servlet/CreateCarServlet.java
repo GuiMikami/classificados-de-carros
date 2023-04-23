26f29e2e29009e57aa09bb99bda2f4ec4ccac5b3
Guilherme Mikami <99701339+GuiMikami@users.noreply.github.com>
2023-04-23 16:07:42 -0300

    atualizando servlet cadastro de carros

--- melfh-cars/src/main/java/br/com/melfhcars/servlet/CreateCarServlet.java
+++ melfh-cars/src/main/java/br/com/melfhcars/servlet/CreateCarServlet.java
@@ -1,6 +1,9 @@
 package br.com.melfhcars.servlet;
 import br.com.melfhcars.dao.CarDAO;
+import br.com.melfhcars.dao.ClienteDAO;
 import br.com.melfhcars.model.Carro;
+import br.com.melfhcars.model.Cliente;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,16 +17,34 @@ public class CreateCarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String carName = request.getParameter("car-name");
-        var car = new Carro();
-        car.setNomeCarro(carName);
+        String nomeCarro = request.getParameter("nomeCarro");
+        String ano = request.getParameter("ano");
+        String km = request.getParameter("quilometragem");
+        String finalPlaca = request.getParameter("finalPlaca");
+        String valor = request.getParameter("valor");
+        String estado = request.getParameter("estado");
+        String uf = request.getParameter("uf");
+        String fotoCarro = request.getParameter("fotoCarro");
 
-        var carDAO = new Carro();
-        //carDAO.createCar(Carro);
+        var carro = new Carro();
+        carro.setNomeCarro(nomeCarro);
+        carro.setAno(ano);
+        carro.setKm(km);
+        carro.setFinalPlaca(finalPlaca);
+        carro.setValor(valor);
+        carro.setEstado(estado);
+        carro.setUf(uf);
+        carro.setFotoCarro(fotoCarro);
 
-        System.out.println(carName);
 
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+        var carroDAO = new CarDAO();
+        carroDAO.createCar(carro);
+
+        System.out.println(nomeCarro);
+
+        request.getRequestDispatcher("cadastroCarro.html").forward(request, response);
+
 
     }
 }
