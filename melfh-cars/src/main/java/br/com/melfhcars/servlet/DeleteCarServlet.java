9599d5dfa88c4e386848ae4bba5d4d808dc260e2
Guilherme Mikami <99701339+GuiMikami@users.noreply.github.com>
2023-05-10 22:00:43 -0300

    implementando DeleteCarServlet

---
+++ melfh-cars/src/main/java/br/com/melfhcars/servlet/DeleteCarServlet.java
@@ -0,0 +1,25 @@
+package br.com.melfhcars.servlet;
+
+import br.com.melfhcars.dao.CarDAO;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/delete-car")
+public class DeleteCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String placa = req.getParameter("placa");
+
+        new CarDAO().deletarCarro(placa);
+
+        resp.sendRedirect("/mostrar-carro");
+
+    }
+}
